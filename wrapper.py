783096c6bfe982ff984dfa08277c609b934d4b60
JongsooLee <jjongs@postech.ac.kr>
2026-02-01 16:51:40 +0900

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -2,33 +2,30 @@ import random
 import gym
 from utils import *
 
-# Random-delay environments
 class DelayedEnv(gym.Wrapper):
-    def __init__(self, env, seed,
-                 init_obs_delayed_steps,
-                 min_obs_delayed_steps,
-                 max_obs_delayed_steps):
+    def __init__(self, args, env, seed):
 
         super(DelayedEnv, self).__init__(env)
-        assert max_obs_delayed_steps > 0, 'args.max_obs_delayed_steps must be greater than 0'
-        assert init_obs_delayed_steps > 0, 'args.init_obs_delayed_steps must be greater than 0'
+        assert args.max_obs_delayed_steps > 0,  'args.max_obs_delayed_steps must be greater than 0'
+        assert args.init_obs_delayed_steps > 0, 'args.init_obs_delayed_steps must be greater than 0'
         self.env.action_space.seed(seed)
+        self._max_episode_steps = self.env._max_episode_steps
 
         self.observation_space = self.env.observation_space
         self.action_space = self.env.action_space
-
-        self._max_episode_steps = self.env._max_episode_steps
+        self.delay_type = args.delay_type
+        self.mu = args.mu
 
         self.obs_buffer    = []
         self.reward_buffer = []
         self.done_buffer   = []
 
-        self.init_obs_delayed_steps = init_obs_delayed_steps
-        self.min_obs_delayed_steps  = min_obs_delayed_steps
-        self.max_obs_delayed_steps  = max_obs_delayed_steps
+        self.init_obs_delayed_steps = args.init_obs_delayed_steps
+        self.min_obs_delayed_steps  = args.min_obs_delayed_steps
+        self.max_obs_delayed_steps  = args.max_obs_delayed_steps
 
-        self.state_idx  = 0
-        self.init_delay = 0
+        self.state_idx  = None
+        self.init_delay = None
         self.init_state = None
 
     def reset(self, seed=None, option=None):
@@ -49,22 +46,22 @@ class DelayedEnv(gym.Wrapper):
         self.done_buffer.append([False, self.state_idx, self.init_delay])
         return init_state
 
-    def step(self, action, no_ops = False):
+    def step(self, action):
         current_obs, current_reward, current_terminated, current_truncated, _ = self.env.step(action)
         current_done = current_terminated or current_truncated
         data_dict = {'current_obs': current_obs, 'current_reward': current_reward, 'current_done': current_done}
 
-        if no_ops == True:
-            raise Exception("Lazy-agent does not perform 'no-ops'. Check if o_init == o_max.")
-
         self.local_time += 1
         self.state_idx += 1
 
-        # Uniform
-        delay = random.randrange(self.min_obs_delayed_steps, self.max_obs_delayed_steps + 1)
-
-        # Possion
-        # delay = poisson_delay(min_delay=self.min_obs_delayed_steps, max_delay=self.max_obs_delayed_steps, lam=3)
+        if self.delay_type == 'uniform':
+            delay = random.randrange(self.min_obs_delayed_steps, self.max_obs_delayed_steps + 1)
+        elif self.delay_type == 'Poisson':
+            if self.mu == None:
+                raise ValueError('Set Poisson parameter')
+            delay = poisson_delay(min_delay=self.min_obs_delayed_steps, max_delay=self.max_obs_delayed_steps, mu=self.mu)
+        else:
+            raise Exception
 
         # meta obs: (a, b, c) -> a= obs, b=generated time, c= how delayed
         meta_obs    = [current_obs,    self.state_idx, delay]
@@ -107,9 +104,3 @@ class DelayedEnv(gym.Wrapper):
 
         return ret_meta_obs_list, ret_meta_reward_list, ret_meta_done_list, data_dict
 
-
-def poisson_delay(min_delay, max_delay, lam=3):
-    while True:
-        delay = np.random.poisson(lam)
-        if min_delay <= delay <= max_delay:
-            return delay
