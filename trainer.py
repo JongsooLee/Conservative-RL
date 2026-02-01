563f231726c9a986f423a771bee0cc8a3802f64b
JongsooLee <jjongs@postech.ac.kr>
2026-02-01 16:52:24 +0900

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -22,16 +22,12 @@ class Trainer:
         self.total_step  = 0
         self.finish_flag = False
 
-        # Useless flags for conservative-agent.
-        self.no_ops = False
-        self.eval_no_ops = False
-
         self.local_step = 0
         self.eval_local_step = 0
 
-        # tags for checking state generation order
-        self.state_tag  = 0
-        self.eval_state_tag  = 0
+        # tags for checking the order of state generation
+        self.state_idx  = 0
+        self.eval_state_idx  = 0
 
         self.init_obs_delayed_steps  = args.init_obs_delayed_steps
         self.min_obs_delayed_steps   = args.min_obs_delayed_steps
@@ -41,22 +37,14 @@ class Trainer:
     def train(self):
         while not self.finish_flag:
             self.episode   += 1
+            self.state_idx  = 1
             self.local_step = 0
-            self.state_tag  = 1
-
-            # temps for observed states, corresponding rewards, and dones.
-            obs_temp     = []
-            reward_temp  = []
-            done_temp    = []
-
-            # reset env. and clear temporal buffer B
             self.delayed_env.reset()
             self.agent.temporary_buffer.clear()
-
-            self.no_ops = False
+            obs_temp, reward_temp, done_temp = [], [], []
             done = False
 
-            # episode starts here.
+            # Episode starts here.
             while not done:
                 self.local_step += 1
                 self.total_step += 1
@@ -66,7 +54,7 @@ class Trainer:
                     obs_list, reward_list, done_list, _ = self.delayed_env.step(action)
                     self.agent.temporary_buffer.actions.append(action) # Put a(t) to the temporary buffer B
 
-                    if len(obs_list) > 0: # append observed states, rewards, dones in temps
+                    if len(obs_list) > 0: # append observed states, rewards, dones
                         for obs, rwd, dn in zip(obs_list, reward_list, done_list):
                             obs_temp.append(obs)
                             reward_temp.append(rwd)
@@ -82,31 +70,24 @@ class Trainer:
                             reward_temp.append(rwd)
                             done_temp.append(dn)
 
-                    # sort observed states in order of generation
-                    obs_temp, reward_temp, done_temp = do_sorting(obs_temp, reward_temp, done_temp)
-
-                    initial_state = obs_temp[0][0] # get initial state
-                    self.state_tag += 1
-
-                    # delete
-                    del obs_temp[0]
-                    del reward_temp[0]
-                    del done_temp[0]
+                    # sort observed states in generated order
+                    obs_temp, reward_temp, done_temp = do_sorting(obs_temp,reward_temp,done_temp)
 
                     self.agent.temporary_buffer.actions.append(action)  # Put a(t) to the temporary buffer B
-                    self.agent.temporary_buffer.states.append(initial_state)  # Put most recent usable state to the temporary buffer B
+                    self.agent.temporary_buffer.states.append(obs_temp[0][0]) # Put most recent usable state to the temporary buffer B
+                    self.state_idx += 1
+
+                    # delete used components
+                    obs_temp, reward_temp, done_temp  = eliminate_head(obs_temp,reward_temp,done_temp)
 
                 else:  # if t > o_max
-                    if self.no_ops == True:
-                        action = np.zeros_like(self.delayed_env.action_space.sample())  # Select the 'no-ops'
-                    else:
-                        # construct augmented state x(t) and select action a(t)
-                        most_recent_usable_state = self.agent.temporary_buffer.states[-1]
-                        first_action_idx = len(self.agent.temporary_buffer.actions) - self.init_obs_delayed_steps
-                        augmented_state = self.agent.temporary_buffer.get_augmented_state(most_recent_usable_state, first_action_idx)
-                        action = self.agent.get_action(augmented_state, evaluation=False)
+                    # construct augmented state x(t) and select action a(t)
+                    most_recent_usable_state = self.agent.temporary_buffer.states[-1]
+                    first_action_idx = len(self.agent.temporary_buffer.actions) - self.init_obs_delayed_steps
+                    augmented_state = self.agent.temporary_buffer.get_augmented_state(most_recent_usable_state, first_action_idx)
+                    action = self.agent.get_action(augmented_state, evaluation=False)
 
-                    obs_list, reward_list, done_list, _ = self.delayed_env.step(action, no_ops = self.no_ops)
+                    obs_list, reward_list, done_list, _ = self.delayed_env.step(action)
 
                     if len(obs_list) > 0:  # append observed states, rewards, dones
                         for obs, rwd, dn in zip(obs_list, reward_list, done_list):
@@ -118,31 +99,23 @@ class Trainer:
                     obs_temp, reward_temp, done_temp = do_sorting(obs_temp, reward_temp, done_temp)
 
                     # for the most recent usable state
-                    if len(obs_temp) > 0 and self.state_tag == obs_temp[0][1]:
-                        self.no_ops = False # flag off
+                    if len(obs_temp) > 0 and self.state_idx == obs_temp[0][1]:
                         next_state = obs_temp[0][0]
                         reward     = reward_temp[0][0]
                         done       = done_temp[0][0]
-                        self.state_tag += 1
-
-                        # delete
-                        del obs_temp[0]
-                        del reward_temp[0]
-                        del done_temp[0]
-
-                        true_done = 0.0 if self.local_step == self.delayed_env._max_episode_steps + self.init_obs_delayed_steps else float(done)
-
                         self.agent.temporary_buffer.actions.append(action)    # Put a(t) to the temporary buffer B
                         self.agent.temporary_buffer.states.append(next_state) # Put most recent usable state to the temporary buffer B
+                        self.state_idx += 1
 
+                        # Delete used ones
+                        obs_temp, reward_temp, done_temp = eliminate_head(obs_temp, reward_temp, done_temp)
+
+                        true_done = 0.0 if self.local_step == self.delayed_env._max_episode_steps + self.init_obs_delayed_steps else float(done)
                         if len(self.agent.temporary_buffer.states) >= (self.init_obs_delayed_steps + 2):
-                            # get x(t-o_max), s(t-o_max), a(t-o_max), x(t+1-o_max), s(t+1-o_max) from temporary buffer B
                             augmented_s, s, a, next_augmented_s, next_s = self.agent.temporary_buffer.get_tuple()
-                            # store [x(t-o_max), s(t-o_max), a(t-o_max), r(t-o_max), x(t+1-o_max), s(t+1-o_max)] in the replay memory D.
                             self.agent.replay_memory.push(augmented_s, s, a, reward, next_augmented_s, next_s, true_done)
                     else:
-                        # When there is no usable state.
-                        self.no_ops = True # flag on
+                        raise ValueError("Check init_obs_delay == max_obs_delay.")
 
                 # Update parameters
                 if self.agent.replay_memory.size >= self.batch_size \
@@ -150,14 +123,13 @@ class Trainer:
                         self.total_step % self.update_every == 0:
 
                     for i in range(self.update_every):
-                        # Train actor and beta critic.
                         self.agent.train()
 
-                # Evaluate.
+                # Evaluate
                 if self.eval_flag and self.total_step % self.eval_freq == 0:
                     self.evaluate()
 
-                # Raise finish flag.
+                # Finish flag
                 if self.total_step == self.max_step:
                     self.finish_flag = True
 
@@ -165,18 +137,14 @@ class Trainer:
     def evaluate(self):
         reward_list = []
         for epi in range(self.eval_episode):
-            episode_reward   = 0
-            eval_obs_temp    = []
-            eval_reward_temp = []
-            eval_done_temp   = []
-
             self.eval_local_step = 0
-            self.eval_state_tag  = 1
+            self.eval_state_idx  = 1
             self.eval_delayed_env.reset()
             self.agent.eval_temporary_buffer.clear()
-            self.eval_no_ops = False
-
+            eval_obs_temp, eval_reward_temp, eval_done_temp = [], [], []
+            episode_reward = 0
             done = False
+
             while not done:
                 self.eval_local_step += 1
                 if self.eval_local_step < self.init_obs_delayed_steps:
@@ -196,53 +164,51 @@ class Trainer:
                             eval_obs_temp.append(obs)
                             eval_reward_temp.append(rwd)
                             eval_done_temp.append(dn)
-                    eval_obs_temp, eval_reward_temp, eval_done_temp = do_sorting(eval_obs_temp, eval_reward_temp, eval_done_temp)
-                    initial_state = eval_obs_temp[0][0]
-                    reward     = eval_reward_temp[0][0]
-                    episode_reward += reward
-                    self.eval_state_tag += 1
-
-                    del eval_obs_temp[0]
-                    del eval_reward_temp[0]
-                    del eval_done_temp[0]
-
+                    eval_obs_temp, eval_reward_temp, eval_done_temp = do_sorting(eval_obs_temp,
+                                                                                 eval_reward_temp,
+                                                                                 eval_done_temp)
+                    episode_reward += eval_reward_temp[0][0]
                     self.agent.eval_temporary_buffer.actions.append(action)
-                    self.agent.eval_temporary_buffer.states.append(initial_state)
-                else:
-                    if self.eval_no_ops == True:
-                        action = np.zeros_like(self.eval_delayed_env.action_space.sample())
-                    else:
-                        most_recent_usable_state = self.agent.eval_temporary_buffer.states[-1]
-                        first_action_idx    = len(self.agent.eval_temporary_buffer.actions) - self.init_obs_delayed_steps
-                        augmented_state     = self.agent.eval_temporary_buffer.get_augmented_state(most_recent_usable_state, first_action_idx)
-                        action = self.agent.get_action(augmented_state, evaluation=True)
+                    self.agent.eval_temporary_buffer.states.append(eval_obs_temp[0][0])
+                    self.eval_state_idx += 1
 
-                    eval_obs_list, eval_reward_list, eval_done_list, info = self.eval_delayed_env.step(action, no_ops = self.eval_no_ops)
+                    # Delete used ones
+                    eval_obs_temp, eval_reward_temp, eval_done_temp = eliminate_head(eval_obs_temp,
+                                                                                     eval_reward_temp,
+                                                                                     eval_done_temp)
+
+                else:
+                    most_recent_usable_state = self.agent.eval_temporary_buffer.states[-1]
+                    first_action_idx    = len(self.agent.eval_temporary_buffer.actions) - self.init_obs_delayed_steps
+                    augmented_state     = self.agent.eval_temporary_buffer.get_augmented_state(most_recent_usable_state, first_action_idx)
+                    action = self.agent.get_action(augmented_state, evaluation=True)
+
+                    eval_obs_list, eval_reward_list, eval_done_list, _ = self.eval_delayed_env.step(action)
                     if len(eval_obs_list) > 0:
                         for obs, rwd, dn in zip(eval_obs_list, eval_reward_list, eval_done_list):
                             eval_obs_temp.append(obs)
                             eval_reward_temp.append(rwd)
                             eval_done_temp.append(dn)
-                    eval_obs_temp, eval_reward_temp, eval_done_temp = do_sorting(eval_obs_temp, eval_reward_temp, eval_done_temp)
+                    eval_obs_temp, eval_reward_temp, eval_done_temp = do_sorting(eval_obs_temp,
+                                                                                 eval_reward_temp,
+                                                                                 eval_done_temp)
 
-                    if len(eval_obs_temp) > 0 and self.eval_state_tag == eval_obs_temp[0][1]:
-                        self.eval_no_ops = False
-                        next_state = eval_obs_temp[0][0]
-                        reward = eval_reward_temp[0][0]
+                    if len(eval_obs_temp) > 0 and self.eval_state_idx == eval_obs_temp[0][1]:
+                        episode_reward += eval_reward_temp[0][0]
                         done = eval_done_temp[0][0]
-                        self.eval_state_tag += 1
-
-                        del eval_obs_temp[0]
-                        del eval_reward_temp[0]
-                        del eval_done_temp[0]
-
                         self.agent.eval_temporary_buffer.actions.append(action)
-                        self.agent.eval_temporary_buffer.states.append(next_state)
-                        episode_reward += reward
+                        self.agent.eval_temporary_buffer.states.append(eval_obs_temp[0][0])
+                        self.eval_state_idx += 1
+
+                        # Delete used ones
+                        eval_obs_temp, eval_reward_temp, eval_done_temp = eliminate_head(eval_obs_temp,
+                                                                                         eval_reward_temp,
+                                                                                         eval_done_temp)
                     else:
-                        self.eval_no_ops = True
+                        raise ValueError("Check init_obs_delay == max_obs_delay.")
+
             reward_list.append(episode_reward)
-        log_to_txt(self.args.env_name, self.args.random_seed,  self.max_obs_delayed_steps, self.total_step, sum(reward_list) / len(reward_list))
+        log_to_txt(self.args.delay_type, self.args.env_name, self.args.random_seed,  self.max_obs_delayed_steps, self.total_step, sum(reward_list) / len(reward_list))
         print("Eval  |  Total Steps {}  |  Episodes {}  |  Average Reward {:.2f}  |  Max reward {:.2f}  |  "
               "Min reward {:.2f}".format(self.total_step, self.episode, sum(reward_list) / len(reward_list), max(reward_list), min(reward_list)))
 
